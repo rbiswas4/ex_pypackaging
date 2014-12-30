d8fce7a81e47f0e4c09b356202c86d4a136e6ad7
R. Biswas <rbiswas4@gmail.com>
2014-12-30 14:44:50 -0800

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#from ez_setup import use_setuptools
+#use_setuptools()
+from setuptools import setup, find_packages
+
+setup(name="example_docs",
+      version="0.0.1dev",
+      description='Simple repo setup to check documentation',
+      long_description=''' ''',
+      packages=['example']
+      )
