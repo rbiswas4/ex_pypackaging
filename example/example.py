84fbdfcc20f1cad9803bd6f7b33a4021b94dee2f
R. Biswas <rbiswas4@gmail.com>
2014-12-30 12:24:45 -0800

    Obtained by running sphinx-quickstart with mostly default configuration:
    Changed variables:
    > Root path for the documentation: doc
    > Project name:
    > Author name(s):
    > Project version: 0.0.1
    > Project release [0.0.1]: 0.0.1dev
    > autodoc: automatically insert docstrings from modules (y/n) [n]: y
    > doctest: automatically test code snippets in doctest blocks (y/n) [n]: y
    > intersphinx: link between Sphinx documentation of different projects
      (y/n) [n]: y
    > todo: write "todo" entries that can be shown or hidden on build
    (y/n) [n]: y
    > coverage: checks for documentation coverage (y/n) [n]: y
    > mathjax: include math, rendered in the browser by MathJax (y/n) [n]: y
    > ifconfig: conditional inclusion of content based on config values
    (y/n) [n]:y
    > viewcode: include links to the source code of documented Python objects (y/n) [n]:
    >  Create Makefile? (y/n) [y]: y
    >  Create Windows command file? (y/n) [y]: n

---
+++ example/example.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+class myclass(object):
+    """
+    Class holding a single member variable x.
+    Parameters
+    ----------
+        val: float, mandatory
+            initial value of the instance variable x
+    Attributes:
+    ----------
+        var: float storing member value x
+    """
+    def __init__(self, val):
+        self.x = val
+    
+    @property
+    def var(self):
+        return self.x
+    def setvar(self, x):
+        """
+        set the value of the member variable var
+        Parameters
+        ----------
+            x : float, mandatory
+                value to which var will be set
+        Returns 
+            None
+        -------
+        """
+        self.x = x
+
+    def square(self):
+        x = self.x
+        self.x = x*x
+
+
+def myfunc(x):
+    """
+    returns the square of the argument x.
+    Parameters
+    ----------
+        x : float, mandatory
+            value that you want to square
+    Returns
+    -------
+        float, desired square of x 
+    """
+    return x *x 
