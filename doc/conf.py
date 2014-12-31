d744cb58eaff2b51d00d0ddb068c25b0d78d2bbf
R. Biswas <rbiswas4@gmail.com>
2014-12-30 19:15:26 -0800

    Returning to where things were a while back

--- doc/conf.py
+++ doc/conf.py
@@ -14,13 +14,14 @@
 
 import sys
 import os
+#from example import example# , second
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 # modifying searchpath
-sys.path.insert(0, os.path.abspath('example'))
+sys.path.insert(0, os.path.abspath('../example'))
 #print sys.path
 
 # -- General configuration ------------------------------------------------
