076dfaddb0cd566c01806ceab066aef763eb55df
R. Biswas <rbiswas4@gmail.com>
2015-01-04 23:47:34 -0800

    Added text docs

--- examples/test.py
+++ examples/test.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python
+"""
+example to read example data packaged with the code
+"""
 import myexpackage.funcmodule as fm
-
+"Just reading a text file without adding paths"
 d  = fm.readmatrix('smallmatrix.dat') 
 print d
